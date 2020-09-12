92d38f09f0a01aa622e8b70d788e8c6c60e77479
Elton Mozart <elton.mozart@gmail.com>
2020-09-12 19:39:44 -0300

    Rotas da aplicação
    O desafio tem uma única rota, mas podemos acrescentar várias rotas com a estrutura utilizada.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import GlobalStyle from './styles/global'
 
-import Home from './pages/Home'
+import Routes from './routes'
 
 const App: React.FC = () => (
-  <>
+  <Router>
     <GlobalStyle />
-    <Home />
-  </>
+    <Routes />
+  </Router>
 )
 
 export default App
